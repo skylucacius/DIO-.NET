dd4f7138fe26e91836960c2e946b92ea91ec6e86
skylucacius <skylucacius@hotmail.com>
2020-12-13 07:40:05 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO_.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
