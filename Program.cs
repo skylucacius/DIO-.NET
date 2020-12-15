e51a52162a2fb1a379d28c166420283baf8171df
skylucacius <skylucacius@hotmail.com>
2020-12-15 20:56:44 -0300

    Hello World

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace DIO_.NET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nOlá, {name}, hoje é {date:d} e a hora atual é {date:t} !");
+            Console.Write("\nPressione qualquer tecla para sair ...");
+            Console.ReadKey(true);
         }
     }
 }
