2587b3d9dbb934de0ef240f61e3d4c66c6e6c677
skylucacius <skylucacius@hotmail.com>
2020-12-16 20:25:15 -0300

    continuando o curso ...

--- Program.cs
+++ Program.cs
@@ -4,14 +4,18 @@ namespace DIO_.NET
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Classe1() {
             Console.WriteLine("\nWhat is your name? ");
             var name = Console.ReadLine();
             var date = DateTime.Now;
             Console.WriteLine($"\nOlá, {name}, hoje é {date:d} e a hora atual é {date:t} !");
             Console.Write("\nPressione qualquer tecla para sair ...");
             Console.ReadKey(true);
+
+        }
+        static void Main(string[] args)
+        {
+            Classe1();
         }
     }
 }
