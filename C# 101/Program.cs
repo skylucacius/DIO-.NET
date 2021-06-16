9ef300904fe47b5abad9130c8c6fc42d8137dd0b
Familia <Familia@DESKTOP-1ROA32V>
2021-06-16 17:14:29 -0300

    atualização de pasta

---
+++ C# 101/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C__101
+{
+    class Program
+    {
+        static void M1() {
+            Console.WriteLine("Por favor, escolha uma opção:");
+            Console.Write("1 - Para Hello World");
+            var option = Console.ReadKey().ToString();
+
+            switch(option)
+            {
+                case "1" :
+                    Console.WriteLine("Hello World ! " + "Hoje é " + DateTime.Now);
+                    break;
+            }
+        }
+        static void M2() {
+            var nomes = new List<String> { "Lucas", "Maria", "João"};
+            foreach (string nome in nomes) {
+                Console.WriteLine($"Olá, {nome}");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            M1();
+            M2();
+        }
+    }
+}
